a642a3211424dc5c9e20890892df932d0f518910
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-10-10 08:46:06 +0600

    todo list add remove icon

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 var input = document.querySelector("#input");
 var btn = document.querySelector("#btn");
 var written_task = document.querySelector(".written_task");
+var work_list = [];
 btn.addEventListener("click", function () {
     if (input.value) {
         var task = input.value;
-        written_task.innerHTML = "<p class=\"bg-sky-400  h-10 m-5 flex justify-center\"> ==>".concat(input.value, "</p>");
-        input.value = "";
+        work_list.push(task); //push to array
+        //creats a new div for each task
+        var taskdiv_1 = document.createElement("div");
+        taskdiv_1.classList = "bg-blue-400  h-10 m-5  flex rounded-3xl justify-center w-200 size-fit";
+        taskdiv_1.textContent = "".concat(work_list.length, ". ==>").concat(task);
+        //cross icon
+        var crossIcon = document.createElement("i");
+        crossIcon.className = "fa-solid fa-xmark pl-5";
+        crossIcon.style.cursor = "pointer";
+        written_task.appendChild(taskdiv_1); //append new task
+        taskdiv_1.appendChild(crossIcon); //append cross icon
+        crossIcon.addEventListener("click", function () {
+            taskdiv_1.remove();
+        });
+        input.value = ""; // to clear the input box
     }
 });
--- script.ts
+++ script.ts
@@ -1,17 +1,52 @@
 let input = document.querySelector("#input") as HTMLInputElement;
 let btn = document.querySelector("#btn") as HTMLButtonElement;
-let written_task=document.querySelector(".written_task");
+let written_task = document.querySelector(".written_task") as HTMLDivElement;
+
+
+let work_list: string[] = [];
 
 
 
 btn.addEventListener("click", () => {
 
     if (input.value) {
-        let task: string | number=input.value;
-         
-        written_task.innerHTML = `<p class="bg-sky-400  h-10 m-5 flex justify-center"> ==>${input.value}</p>`;
+        let task: string | number = input.value;
 
-        input.value="";
+
+        work_list.push(task); //push to array
+
+
+        //creats a new div for each task
+        let taskdiv = document.createElement("div");
+        taskdiv.classList = "bg-blue-400  h-10 m-5  flex rounded-3xl justify-center w-200 size-fit";
+        taskdiv.textContent = `${work_list.length}. ==>${task}`;
+        
+
+
+        //cross icon
+        let crossIcon=document.createElement("i");
+        crossIcon.className="fa-solid fa-xmark pl-5";
+        crossIcon.style.cursor="pointer";
+
+
+        written_task.appendChild(taskdiv);  //append new task
+        taskdiv.appendChild(crossIcon); //append cross icon
+
+
+
+        crossIcon.addEventListener("click",()=>{
+            taskdiv.remove();
+            
+        });
+        
+        
+
+        
+
+
+        input.value = "";// to clear the input box
+
+
 
     }
 
