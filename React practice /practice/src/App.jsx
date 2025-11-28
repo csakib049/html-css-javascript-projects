13d099748d0ccb4943d420af84336c75a6553bff
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-11-28 09:34:47 +0600

    practice theme change

--- React practice /practice/src/App.jsx
+++ React practice /practice/src/App.jsx
@@ -1,15 +1,32 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 import Navbar from './Navbar';
+
+// const App = () => {
+
+//   const [theme,setTheme]=useState("Light");
+
+
+//   return (
+//     <div>
+//       <h1>theme is {theme}</h1>
+//       <Navbar theme={theme}  setTheme={setTheme}/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from 'react'
 
 const App = () => {
 
-  const [theme,setTheme]=useState("Light");
+  const [theme,setTheme]=useState("light");
 
 
   return (
     <div>
-      <h1>theme is {theme}</h1>
-      <Navbar theme={theme}  setTheme={setTheme}/>
+      <h1>The theme is {theme}</h1>
+      <Navbar theme={theme} setTheme={setTheme}/>
     </div>
   )
 }
--- React practice /practice/src/Navbar.jsx
+++ React practice /practice/src/Navbar.jsx
@@ -1,14 +1,36 @@
+// import React from 'react'
+
+// const Navbar = (props) => {
+
+//     function changeTheme(){
+//         if(props.theme=="Light"){
+//             props.setTheme("Dark");
+//         }else{
+//             props.setTheme("Light");
+//         }
+//     }
+
+//   return (
+//     <div>
+//       <button onClick={changeTheme}>change theme</button>
+//     </div>
+//   )
+// }
+
+// export default Navbar
+
 import React from 'react'
 
 const Navbar = (props) => {
 
-    function changeTheme(){
-        if(props.theme=="Light"){
-            props.setTheme("Dark");
-        }else{
-            props.setTheme("Light");
-        }
+  function changeTheme(){
+    if(props.theme=="light"){
+      props.setTheme("dark");
+    }else{
+      props.setTheme("light");
     }
+  }
+
 
   return (
     <div>
