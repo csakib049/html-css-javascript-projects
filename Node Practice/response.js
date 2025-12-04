89e42bb08ea0b14daec5c048b3f908bfb893dc2a
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-12-04 20:18:51 +0600

    practicing node

--- Node Practice/response.js
+++ Node Practice/response.js
@@ -1,21 +1,12 @@
 const http = require('http');
-
-const userData=[
-    { name:"sakib", age:24},
-    { name:"sakibbbb", age:2445},
-    { name:"sakibaaa", age:24345},
-]
-
 http.createServer((req,res)=>{
-    res.setHeader("Content-Type","application/JSON");
-    res.write(JSON.stringify(userData)); 
-    // convirts the object array into JSON string 
+    res.setHeader("Content-Type","text/html");
+
+    console.log(req.url);
+
+    if(req.url=='/loin'){
+        res.write("<h1> login page</h1>");
+    }
+    
     res.end();
-
-}).listen(4100);
-
-
-//  **Description :  this is a server / Api so its sending data to the 
-//  client  thats why its convirting the js object --> JSON srting 
-//  (because api's always sends JSON file)
-
+}).listen(5600);
