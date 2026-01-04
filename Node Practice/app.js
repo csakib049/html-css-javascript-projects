0acc5eedd742d88b69a94f6f3fbda4d124e1b4be
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-01-04 15:49:57 +0600

    practice node js

--- Node Practice/app.js
+++ Node Practice/app.js
@@ -1,10 +1,16 @@
-const http=require('http');
+// const http = require('http');
+
+// http.createServer((req,res)=>{
+     
+//      res.write("<h1> hello sakib </h1>");
+//      res.end("hello");// ending the server is compulsory 
+// }).listen(4800);
+
+
+const http = require('http');
 
 http.createServer((req,res)=>{
 
-    res.write("sakib hi baby ");
-
+    res.write("<h1>is it working ......???</h1>");
     res.end();
 }).listen(4800);
-
-
--- React practice /practice/src/Navbar.jsx
+++ React practice /practice/src/Navbar.jsx
@@ -1,40 +1,9 @@
-// import React from 'react'
-
-// const Navbar = (props) => {
-
-//     function changeTheme(){
-//         if(props.theme=="Light"){
-//             props.setTheme("Dark");
-//         }else{
-//             props.setTheme("Light");
-//         }
-//     }
-
-//   return (
-//     <div>
-//       <button onClick={changeTheme}>change theme</button>
-//     </div>
-//   )
-// }
-
-// export default Navbar
-
 import React from 'react'
 
-const Navbar = (props) => {
-
-  function changeTheme(){
-    if(props.theme=="light"){
-      props.setTheme("dark");
-    }else{
-      props.setTheme("light");
-    }
-  }
-
-
+const Navbar = () => {
   return (
     <div>
-      <button onClick={changeTheme}>change theme</button>
+      
     </div>
   )
 }
