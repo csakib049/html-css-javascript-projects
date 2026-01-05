a4492f785a388f6d4668e8ba5476fa21d30c581a
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-01-05 10:21:31 +0600

    creating server

--- Node Practice/colors.js
+++ Node Practice/colors.js
@@ -1,15 +1,1 @@
-const colors = require('colors');
-colors.setTheme({
-  error:'red',
-  debug:'blue',
-  warn:'yellow',
-  help: 'cyan'
-});
-   
-console.log(colors.bgMagenta("Hi this is sakib"));      
-console.log(colors.bgGreen("Hi this is sakib"));      
-console.log(colors.bgCyan("Hi this is sakib"));      
-console.log('this is an error '.error);
-console.log('this is a debug '.debug);
-console.log('this is a help'.help);
-console.log('this my last warning '.warn);
+const http=
--- Node Practice/response.js
+++ Node Practice/response.js
@@ -1,1 +1,7 @@
-console.log("sakib");
+const http = require('http');
+
+http.createServer((req,res)=>{
+    console.log(req.headers.host); // its running in the server not in the browser
+    res.write("<h1>Hi sakib </h1>");
+    res.end();
+}).listen(5600);
