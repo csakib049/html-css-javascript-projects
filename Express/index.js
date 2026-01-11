b6a5a289cbd18a3673e5bb441ed7917a693276ad
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-01-11 11:55:43 +0600

    practiceing middleware

--- Express/index.js
+++ Express/index.js
@@ -1,21 +1,21 @@
 import express from 'express';
-import path from 'path';
+const app = express();
 
-const app = express();  // convirts express function into an object 
+app.use((req,resp,next)=>{
+   console.log("user is accessing "+req.url+" page");
+   next();
+});
+
 
 app.get('/',(req,resp)=>{
-    const abspath = path.resolve('view','home.html');
-    resp.sendFile(abspath);
+    resp.send("This is home page . ");
 });
-
 app.get('/about',(req,resp)=>{
-    const abspath = path.resolve('view','about.html');
-    resp.sendFile(abspath);
+    resp.send("This is about page . ");
+});
+app.get('/contact',(req,resp)=>{
+    resp.send("This is contact page . ");
 });
 
-app.get('/login',(req,resp)=>{
-    const abspath = path.resolve('view','login.html');
-    resp.sendFile(abspath);
-});
 
 app.listen(3200);
