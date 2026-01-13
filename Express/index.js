e7b05be1ff5842baa25651db220182717e548418
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-01-13 08:19:44 +0600

    error Handling

--- Express/index.js
+++ Express/index.js
@@ -1,21 +1,25 @@
-import express from 'express';
-const app = express();
-
-app.use((req,resp,next)=>{
-   console.log("user is accessing "+req.url+" page");
-   next();
-});
-
+import express from 'express'
+const app=express();
 
 app.get('/',(req,resp)=>{
-    resp.send("This is home page . ");
+    resp.send('<h1>This is home page.</h1>');
 });
 app.get('/about',(req,resp)=>{
-    resp.send("This is about page . ");
+    resp.send1('<h1>This is about page.</h1>');
 });
-app.get('/contact',(req,resp)=>{
-    resp.send("This is contact page . ");
+app.get('/error',(req,resp)=>{
+    resp.send('<h1>This is error page.</h1>');
 });
+
+
+
+function errorHandling(error,req,resp,next){
+    resp.status(error.status||500).send("Try this after some time");
+}
+
+app.use(errorHandling);
+
+
 
 
 app.listen(3200);
