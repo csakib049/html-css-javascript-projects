de2116d90d9df7edcfd62c5baa8cb212d9604223
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-01-14 20:51:53 +0600

    Dynamic Routes

--- Express/index.js
+++ Express/index.js
@@ -1,8 +1,26 @@
 import express from 'express';
- import { handleUser } from './controller/userController';
-const app = express();
+const app= express();
 
-app.set('view engine','ejs');
-app.get('/user',handleUsers);
+app.get('/',(req,resp)=>{
+    const user=['anil','sakib','sam','sidhu'];
+    let data=`<ul>`;
+
+    for(let i=0;i<user.length;i++){
+        data+=`<li><a href="user/${user[i]}">${user[i]}</a></li>`
+        console.log(user[i]);
+    }
+    data+='</ul>'
+    resp.send(data);
+});
+
+app.get("/user/:name",(req,resp)=>{
+    console.log(req.params.name);
+     const userName=req.params.name;
+    resp.send(`This is ${userName}'s profile page.`);
+});
+
+
+
+
 
 app.listen(3200);
