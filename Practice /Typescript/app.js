1b044fc37f99fef830b56ab467d6c2d2933d211c
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-03-24 08:30:58 +0600

    practicing typescript

---
+++ Practice /Typescript/app.js
@@ -0,0 +1,6 @@
+"use strict";
+var num1 = 24;
+var num2 = 10;
+var num3 = "30saasdf";
+var total = num1 + num2 + num3;
+console.log(total);
---
+++ Practice /Typescript/app.ts
@@ -0,0 +1,8 @@
+var num1:number=24;
+var num2:number=10;
+
+var num3:string="30saasdf";
+
+var total:number=num1+num2+num3;
+
+console.log(total);
