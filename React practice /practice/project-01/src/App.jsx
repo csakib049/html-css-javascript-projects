c3ebff0b1dd1eb333074c56010fdbc993b38bafc
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-04-12 11:42:48 +0600

    practicing react router dom

--- React practice /practice/project-01/src/App.jsx
+++ React practice /practice/project-01/src/App.jsx
@@ -1,35 +1,31 @@
-import axios from 'axios'
-import React, { useState } from 'react'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import { Link } from 'react-router-dom'
 
 const App = () => {
-
-  const [data, setData] = useState([]);
-
-  const btnclicked = async () => {
-
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-
-
-    setData(response.data);
-
-     console.log(response.data);
-     
-
-  }
-
   return (
     <div>
 
-      <button onClick={btnclicked}>fetch data</button>
+      <nav><h1>This is a Navbar</h1>  </nav>
 
-      <div>
-        {
-          data.map(function (elem, idx) {
+     
+     <Link to='/'>Home</Link>
+     <Link to='/about'>About</Link>
+     <Link to='/contact'>Contact</Link>
 
-            return <h3>Hello {elem.title} {idx}</h3>
-          })
-        }
-      </div>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+
+      </Routes>
+
+      <footer><h1>This is a footer</h1> </footer>
+
     </div>
   )
 }
