b49a95809d54f2780c6cc3af3d4e20a1f3dfc209
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-06-11 06:17:46 +0600

    fixing

--- Node Practice/Node_3/src/controllers/auth.controller.js
+++ Node Practice/Node_3/src/controllers/auth.controller.js
@@ -11,39 +11,46 @@ async function registerUser(req, res) {
 
 
     //this funciton checks if the user already exixts based on the unique email
-    const isUserAlreadyExists=await userModel.findOne({
+    const isUserAlreadyExists = await userModel.findOne({
         email
-    }) 
+    })
 
 
     // 409 --> conflict {User already exists}
-    if (isUserAlreadyExists){ 
+    if (isUserAlreadyExists) {
         return res.status(409).json({
-            message:"User already exists"
+            message: "User already exists"
         })
     }
 
 
     //you are creating a new user in your database using Mongoose 
     const user = await userModel.create({ //userModel: This is the schema/blueprint for your user data.
-        username,email,password
+        username, email, password
     })
 
 
 
-//jwt.sign():This function creates a unique (the token) for the user.
-    const token =jwt.sign({                         
-        id:user._id//This is the data you want to "encode" into the token. By storing the user's unique ID here, the server will know exactly who owns this token when they send it back later.
-    },process.env.JWT_SECRET)  //(process.env.JWT_SECRET): This is a private key stored in your environment variables. Think of it as a digital wax seal. Only your server knows this secret, which prevents hackers from creating their own valid tokens.
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET);
+
+    if(!token){
+        return res.status(400).json({
+            message:"Failded to create Token."
+        })
+    }
 
 
     //           "key",value
-    res.cookie("token",token) //"Hey, take this secret token and store it in user's browser 'cookie jar' automatically." 
+    res.cookie("token", token) //"Hey, take this secret token and store it in user's browser 'cookie jar' automatically." 
 
+    
     res.status(201).json({
-        message:"User registered successfully",
+        message: "User registered successfully",
         user,
-        
+        token
+
     })
 }
 
